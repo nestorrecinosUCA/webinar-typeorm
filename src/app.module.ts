8b3435a2dadfa5e80081469fadfb83c5f74d07f5
Nestor Recinos <00083120@uca.edu.sv>
2022-04-23 23:09:06 -0600

    :sparkles: add configuration for coneccting db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/entities/user.entity';
+import { Db } from 'typeorm';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: process.env.DB_HOSTS,
+    port: Number(process.env.DB_PORT),
+    username: 'username',
+    password: '',
+    database: process.env.DB_DATABASE,
+    useUnifiedTopology: true,
+    entities: [User],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
