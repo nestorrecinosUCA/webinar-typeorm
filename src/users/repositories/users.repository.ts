d36cd706d10b41efb396604bccaec51d3f9398e4
Nestor Recinos <00083120@uca.edu.sv>
2022-04-24 13:05:12 -0600

    :sparkles: Add repository on modules

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,12 +1,12 @@
-import { EntityRepository, MongoRepository } from "typeorm";
+import { User } from "src/entities/user.entity";
+import { EntityRepository, MongoRepository, Repository } from "typeorm";
 import { CreateUserDto } from "../dto/create-user.dto";
-import { User } from "../entities/user.entity";
 
 @EntityRepository(User)
-export class UserRepository extends MongoRepository<User> {
+export class UsersRepository extends MongoRepository<User> {
   public async createUser(createUserDto: CreateUserDto): Promise<User>{
     const newUser = this.create(createUserDto);
     const userSaved = await this.save(newUser);
     return userSaved;
-  }
+  } 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MongoRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '../entities/user.entity';
+import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(User)
+  private readonly userRepository: UsersRepository) { }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
