4c3f8fcbe32ec080f27df54dc529dc919a5a7d27
Nestor Recinos <00083120@uca.edu.sv>
2022-04-24 13:29:13 -0600

    :sparkles: add config for class validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import "reflect-metadata";
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
   const port = 3000;
   await app.listen(port);
   console.log(`Server running on port http://localhost:${port}`);
