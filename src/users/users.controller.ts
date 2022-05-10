e515cd911e13a864534207b6296a3a9ba38bded6
Nestor Recinos <00083120@uca.edu.sv>
2022-05-09 23:32:13 -0600

    :memo: Add Doc to Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,32 +11,46 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ObjectID } from 'typeorm';
+import { ApiResponse } from '@nestjs/swagger';
+import { User } from 'src/entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('')
+  @ApiResponse({ status: 201, type: User })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiResponse({ status: 200, type: [User] })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':email')
+  @ApiResponse({ status: 200, type: User }) // El type hace referencia al objeto que se espera obtener
+  @ApiResponse({ status: 404, description: 'Not found' })
   findOne(@Param('email') email: string) {
     return this.usersService.findOne(email);
   }
 
   @Patch(':id')
+  @ApiResponse({ status: 200, type: User })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'Not found' })
   update(@Param('id') id: ObjectID, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 201, description: 'User deleted' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'Not found' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
