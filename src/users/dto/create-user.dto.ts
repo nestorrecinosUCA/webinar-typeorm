bb07d1d7d02dcbe4e51345fb754a9fdf9d1b4871
Nestor Recinos <00083120@uca.edu.sv>
2022-05-09 23:12:08 -0600

    :memo: Add documentation on DTOs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNumber,
@@ -7,16 +8,20 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'The name of the person'})
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'The email of the person'})
   @IsEmail()
   @MinLength(3, { message: 'email must have at least 3 characters' })
   email: string;
 
+  @ApiProperty({ description: 'The age of the person', minimum: 1})
   @IsNumber()
   age: number;
 
+  @ApiProperty({ description: 'The phone number of the person' })
   @IsString()
   @MaxLength(8)
   phone: string;
