ab6b87aa30f166d05e2e6a5b7d5fcf30f2839240
Nestor Recinos <00083120@uca.edu.sv>
2022-05-09 22:57:21 -0600

    :memo: Add configuration for create the documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Example title for API')
+    .setDescription('This API has a CRUD as example in order to learn the basis for NestJS, MongoDB and TypeORM')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document); // "api/v1" es la ruta donde se almacenara la documentacion
+  
   const port = 3000;
   await app.listen(port);
   console.log(`Server running on port http://localhost:${port}`);
