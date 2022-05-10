e859ab4fd0bcab5961ac8a307f2c0cdb916defee
Nestor Recinos <00083120@uca.edu.sv>
2022-05-09 23:30:36 -0600

    :memo: Add Doc to Entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -8,19 +9,26 @@ import {
 @Entity()
 export class User {
   @ObjectIdColumn()
+  @ApiHideProperty()
   id: ObjectID;
 
   @Column({ nullable: false })
+  @ApiProperty()
   name: string;
 
   @Column({ unique: true, nullable: false })
+  @ApiProperty()
   email: string;
 
   @Column({ nullable: false })
+  @ApiProperty({ default: 1 })
   age: number;
 
   @Column({ nullable: true })
+  @ApiProperty()
   phone: string;
+
   @CreateDateColumn()
+  @ApiProperty()
   createdAt: Date;
 }
