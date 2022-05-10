9838d403542127add3c2519811aaca84d9f20864
Nestor Recinos <00083120@uca.edu.sv>
2022-05-09 23:12:38 -0600

    :recycle: Refactor Code

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -8,16 +8,16 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ description: 'The name of the person'})
+  @ApiProperty({ description: 'The name of the person' })
   @IsString()
   name: string;
 
-  @ApiProperty({ description: 'The email of the person'})
+  @ApiProperty({ description: 'The email of the person' })
   @IsEmail()
   @MinLength(3, { message: 'email must have at least 3 characters' })
   email: string;
 
-  @ApiProperty({ description: 'The age of the person', minimum: 1})
+  @ApiProperty({ description: 'The age of the person', minimum: 1 })
   @IsNumber()
   age: number;
 
